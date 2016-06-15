0477914372ee00fc061fcc96d750bee88bbd7a4a
keligula <keldonger@gmail.com>
2016-06-14 18:14:46 -0700

    Update CMSController.cs

--- PageManagerFeature/Controllers/CMSController.cs
+++ PageManagerFeature/Controllers/CMSController.cs
@@ -1,11 +1,11 @@
-﻿using Sabio.Web.Models.ViewModels;
+﻿using Spright.Web.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Sabio.Web.Controllers.CMS
+namespace Spright.Web.Controllers.CMS
 {
     [RoutePrefix("cms")]
     public class CMSController : BaseController
