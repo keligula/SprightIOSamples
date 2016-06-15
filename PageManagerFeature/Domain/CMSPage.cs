d637c4f94b6320db37f91437d951f034650c758a
keligula <keldonger@gmail.com>
2016-06-14 18:15:17 -0700

    Update CMSPage.cs

--- PageManagerFeature/Domain/CMSPage.cs
+++ PageManagerFeature/Domain/CMSPage.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Sabio.Web.Domain
+namespace Spright.Web.Domain
 {
     public class CMSPage
     {
