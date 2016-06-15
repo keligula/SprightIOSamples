f86276bb4999ed1b2ed3c3b771eb16de56c3ae09
keligula <keldonger@gmail.com>
2016-06-14 18:23:36 -0700

    Update PagesManagerService.cs

--- PageManagerFeature/Service/PagesManagerService.cs
+++ PageManagerFeature/Service/PagesManagerService.cs
@@ -29,10 +29,7 @@ namespace Spright.Web.Services
                    paramCollection.AddWithValue("@PageDescription", model.PageDescription);
                    paramCollection.AddWithValue("@PageTemplate", model.PageTemplate);
                    paramCollection.AddWithValue("@PageIsActive", model.PageIsActive);
-                   paramCollection.AddWithValue("@WebsiteId", model.WebsiteId);
-                   paramCollection.AddWithValue("@Slug", model.Slug);
-                   paramCollection.AddWithValue("@EntityId", model.EntityId);
-
+                  
                    SqlParameter p = new SqlParameter("@Id", System.Data.SqlDbType.Int);
                    p.Direction = System.Data.ParameterDirection.Output;
 
@@ -48,70 +45,6 @@ namespace Spright.Web.Services
             return id;
         }
 
-        // POST: Create new page with websiteId
-        public int InsertWithWebsiteId(CMSAddPageRequestModel model)
-
-        {
-            int id = 0;
-
-            DataProvider.ExecuteNonQuery(GetConnection, "dbo.CMSPages_InsertWithWebsiteId"
-               , inputParamMapper: delegate (SqlParameterCollection paramCollection)
-               {
-                   paramCollection.AddWithValue("@PageName", model.PageName);
-                   paramCollection.AddWithValue("@PageDescription", model.PageDescription);
-                   paramCollection.AddWithValue("@PageTemplate", model.PageTemplate);
-                   paramCollection.AddWithValue("@PageIsActive", model.PageIsActive);
-                   paramCollection.AddWithValue("@WebsiteId", model.WebsiteId);
-                   paramCollection.AddWithValue("@Slug", model.Slug);
-                   paramCollection.AddWithValue("@EntityId", model.EntityId);
-
-                   SqlParameter p = new SqlParameter("@Id", System.Data.SqlDbType.Int);
-                   p.Direction = System.Data.ParameterDirection.Output;
-
-                   paramCollection.Add(p);
-
-               }, returnParameters: delegate (SqlParameterCollection param)
-               {
-                   int.TryParse(param["@Id"].Value.ToString(), out id);
-               }
-               );
-
-            return id;
-        }
-
-        // insert cms page attributes
-        public void cmsPageAttrInsert(CmsPageAttributesRequestModel model, int pageId)
-        {
-
-            DataProvider.ExecuteNonQuery(GetConnection, "dbo.CmsPageAttributes_Insert"
-               , inputParamMapper: delegate (SqlParameterCollection paramCollection)
-               {
-                   paramCollection.AddWithValue("@CmsPageId", pageId);
-                   paramCollection.AddWithValue("@Filter", model.Filter);
-                   paramCollection.AddWithValue("@AttributeId", model.Id);
-
-               }, returnParameters: delegate (SqlParameterCollection param)
-               {
-               }
-               );
-        }
-
-        // insert cms page metaTags
-        public void cmsMetaTagsInsert(CmsPageMetaTagRequestModel model, int pageId)
-        {
-
-            DataProvider.ExecuteNonQuery(GetConnection, "dbo.CMSPages_MetaTag_Insert"
-               , inputParamMapper: delegate (SqlParameterCollection paramCollection)
-               {
-                   paramCollection.AddWithValue("@metaKey", model.metaKey);
-                   paramCollection.AddWithValue("@metaValue", model.metaValue);
-                   paramCollection.AddWithValue("@pageId", pageId);
-
-               }, returnParameters: delegate (SqlParameterCollection param)
-               {
-               }
-               );
-        }
 
         // GET: List the Pages Index
         public List<Domain.CMSPage> List()
@@ -144,35 +77,6 @@ namespace Spright.Web.Services
             return returnList;
         }
 
-        // GET: Select Page by slug AND websiteId
-        public CMSPage GetPageBySlugAndWebsiteId(string slug, int pagesId)
-        {
-            CMSPage p = null;
-
-            DataProvider.ExecuteCmd(GetConnection, "dbo.CMSPages_SelectBySlugAndWebsiteId"
-              , inputParamMapper: delegate (SqlParameterCollection paramCollection)
-              {
-                  paramCollection.AddWithValue("@WebsiteId", pagesId);
-                  paramCollection.AddWithValue("@Slug", slug);
-
-              }, map: delegate (IDataReader reader, short set)
-
-              {
-                  p = new CMSPage();
-                  int startingIndex = 0;
-
-                  p.Id = reader.GetSafeInt32(startingIndex++);
-                  p.PageName = reader.GetSafeString(startingIndex++);
-                  p.PageDescription = reader.GetSafeString(startingIndex++);
-                  p.PageTemplate = reader.GetSafeInt32(startingIndex++);
-                  p.PageIsActive = reader.GetSafeBool(startingIndex++);
-                  p.WebsiteId = reader.GetSafeInt32(startingIndex++);
-                  p.Slug = reader.GetSafeString(startingIndex++);
-              }
-              );
-
-            return p;
-        }
 
         // GET: Select Page by ID
         public Domain.CMSPage GetPageById(int pagesId)
@@ -226,40 +130,6 @@ namespace Spright.Web.Services
             return p;
         }
 
-        // GET: Select Page by websiteID 
-        public List<CMSPage> GetPageByWebsiteId(int websiteId)
-        {
-            List<CMSPage> list = null;
-
-            DataProvider.ExecuteCmd(GetConnection, "dbo.CMSPages_SelectByWebsiteId"
-              , inputParamMapper: delegate (SqlParameterCollection paramCollection)
-              {
-                  paramCollection.AddWithValue("@websiteId", websiteId);
-
-              }, map: delegate (IDataReader reader, short set)
-
-              {
-                  CMSPage p = new CMSPage();
-                  int startingIndex = 0;
-
-                  p.Id = reader.GetSafeInt32(startingIndex++);
-                  p.PageName = reader.GetSafeString(startingIndex++);
-                  p.PageDescription = reader.GetSafeString(startingIndex++);
-                  p.PageDescription = Regex.Replace(p.PageDescription, @"<[^>]*>", String.Empty); // remove html tags
-                  p.PageTemplate = reader.GetSafeInt32(startingIndex++);
-                  p.PageIsActive = reader.GetSafeBool(startingIndex++);
-                  p.Slug = reader.GetSafeString(startingIndex++);
-
-                  if (list == null)
-                  {
-                      list = new List<CMSPage>();
-                  }
-                  list.Add(p);
-              }
-              );
-
-            return list;
-        }
 
         // PUT: Update Pages by Id
         public void Update(CMSAddPageRequestModel model, int pagesId)
@@ -295,88 +165,6 @@ namespace Spright.Web.Services
 
                }
                );
-        }
-
-        // DELETE Pages by Id
-        public void DeletePagesAttributes(int pagesId)
-        {
-            DataProvider.ExecuteNonQuery(GetConnection, "dbo.CmsPageAttributes_DeleteById"
-              , inputParamMapper: delegate (SqlParameterCollection paramCollection)
-              {
-                  paramCollection.AddWithValue("@CmsPageId", pagesId);
-
-              }, returnParameters: delegate (SqlParameterCollection param)
-              {
-
-              }
-               );
-        }
-
-        // DELETE Pages by Id
-        public void DeletePagesMeta(int pagesId)
-        {
-            DataProvider.ExecuteNonQuery(GetConnection, "dbo.CmsPageMeta_DeleteById"
-              , inputParamMapper: delegate (SqlParameterCollection paramCollection)
-              {
-                  paramCollection.AddWithValue("@CmsPageId", pagesId);
-
-              }
-              );
-        }
-
-        // check slug
-        public string checkSlug(CmsPageSlugCheckRequestModel model)
-        {
-            string slugVal = null;
-
-            DataProvider.ExecuteCmd(GetConnection, "dbo.CMSPages_checkSlug"
-              , inputParamMapper: delegate (SqlParameterCollection paramCollection)
-              {
-                  paramCollection.AddWithValue("@WebsiteId", model.websiteId);
-                  paramCollection.AddWithValue("@Slug", model.slug);
-
-              }, map: delegate (IDataReader reader, short set)
-
-              {
-                  int startingIndex = 0;
-
-                  slugVal = reader.GetSafeString(startingIndex++);
-
-              }
-              );
-
-            return slugVal;
-        }
-
-        // GET: List Attributes by PageId
-        public List<CmsPageAttributes> ListAttrByPageId(int pageId)
-        {
-            List<CmsPageAttributes> List = null;
-
-            DataProvider.ExecuteCmd(GetConnection, "dbo.CMSPageAttributes_ListAttr_By_PageId"
-              , inputParamMapper: delegate (SqlParameterCollection paramCollection)
-              {
-                  paramCollection.AddWithValue("@PageId", pageId);
-
-              }, map: delegate (IDataReader reader, short set)
-
-              {
-
-                  int startingIndex = 0;
-                  CmsPageAttributes attr = new CmsPageAttributes();
-                  attr.EntityId = reader.GetSafeInt32(startingIndex++);
-                  attr.CmsPageId = reader.GetSafeInt32(startingIndex++);
-                  attr.Id = reader.GetSafeInt32(startingIndex++);
-                  attr.Filter = reader.GetSafeString(startingIndex++);
-                  if (List == null)
-                  {
-                      List = new List<CmsPageAttributes>();
-                  }
-                  List.Add(attr);
-              }
-              );
-
-            return List;
         }
 
     }
