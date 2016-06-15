d0936f98e3e832d2ee382e240ab95a7b7f9ebc17
keligula <keldonger@gmail.com>
2016-06-15 09:58:56 -0700

    Update IPagesManagerService.cs

--- PageManagerFeature/Interface/IPagesManagerService.cs
+++ PageManagerFeature/Interface/IPagesManagerService.cs
@@ -13,19 +13,10 @@ namespace Spright.Web.Services.Interfaces
     {
 
         int Insert(CMSAddPageRequestModel model);
-        int InsertWithWebsiteId(CMSAddPageRequestModel model);
         List<CMSPage> List();
-        CMSPage GetPageBySlugAndWebsiteId(string slug, int pagesId);
         CMSPage GetPageById(int pagesId);
-        List<CMSPage> GetPageByWebsiteId(int websiteId);
         void Update(CMSAddPageRequestModel model, int pagesId);
-        void DeletePagesAttributes(int pagesId);
-        void DeletePagesMeta(int pagesId);
         void DeletePagesById(int pagesId);
-        string checkSlug(CmsPageSlugCheckRequestModel model);
-        void cmsPageAttrInsert(CmsPageAttributesRequestModel model, int pageId);
-        void cmsMetaTagsInsert(CmsPageMetaTagRequestModel model, int pageId);
-        List<CmsPageAttributes> ListAttrByPageId(int pageId);
-
+        
     }
 }
